26f81cf35283657104530420a88f32afe44786b8
Rafał Martyna <rmartyna94@gmail.com>
2015-04-03 18:23:57 +0200

    More testing.

--- scraper.py
+++ scraper.py
@@ -13,37 +13,42 @@ import sys
 # ADD GUI
 # LEARN HOW TO SCRAPE TOTAL KUDOSU EARNED(FAKE JAVASCRIPT)
 
+def init():
+    global LOGIN_DATA, FAVOURITED_TIMES_, BEATMAP_ID_, STAR_DIFFICULTY_, GAME_MODE_, \
+            ARTIST_, TITLE_, USER_ID_, RANKED_, PENDING_, GRAVEYARDED_, PP_RANK_, CREATOR_, \
+            INVALID_CHARACTERS_, FIRST_PAGE, LAST_PAGE, MIN_FAVOURITED, MIN_DIFFICULTY, \
+            MIN_RANKED, MIN_NON_RANKED, MIN_PP_RANK, DOWNLOAD_FOLDER, ERROR_LOG
 
-LOGIN_DATA = {
-    'username': 'krur',
-    'password': 'zlototopotega',
-    'login': 'Login'
-}
-FAVOURITED_TIMES_ = re.compile(r'<div><b>Favourited ([0-9]+) times?</b> in total')
-BEATMAP_ID_ = re.compile(r'a href=\'/s/([0-9]+)\'')
-STAR_DIFFICULTY_ = re.compile(r'\"difficultyrating\":\"([0-9\.]+)\"')
-GAME_MODE_ = re.compile(r'\"mode\":\"([0123])\"')
-ARTIST_ = re.compile(r'\"artist\":\"([^\"]+)\"')
-TITLE_ = re.compile(r'\"title\":\"([^\"]+)\"')
-USER_ID_ = re.compile(r'\"user_id\":\"([0-9]+)\"')
-RANKED_ = re.compile(r'Ranked & Approved Beatmaps \(([0-9]+)\)')
-PENDING_ = re.compile(r'Pending Beatmaps \([0-9]+\)')
-GRAVEYARDED_ = re.compile(r'Graveyarded Beatmaps \([0-9]+\)')
-PP_RANK_ = re.compile(r'\"pp_rank\":\"([0-9]+)\"')
-CREATOR_ = re.compile(r'\"creator\":\"([^\"]+)\"')
-INVALID_CHARACTERS_ = re.compile(r'[\\/\?:\*<>|"]')
+    LOGIN_DATA = {
+        'username': 'krur',
+        'password': 'zlototopotega',
+        'login': 'Login'
+    }
+    FAVOURITED_TIMES_ = re.compile(r'<div><b>Favourited ([0-9]+) times?</b> in total')
+    BEATMAP_ID_ = re.compile(r'a href=\'/s/([0-9]+)\'')
+    STAR_DIFFICULTY_ = re.compile(r'\"difficultyrating\":\"([0-9\.]+)\"')
+    GAME_MODE_ = re.compile(r'\"mode\":\"([0123])\"')
+    ARTIST_ = re.compile(r'\"artist\":\"([^\"]+)\"')
+    TITLE_ = re.compile(r'\"title\":\"([^\"]+)\"')
+    USER_ID_ = re.compile(r'\"user_id\":\"([0-9]+)\"')
+    RANKED_ = re.compile(r'Ranked & Approved Beatmaps \(([0-9]+)\)')
+    PENDING_ = re.compile(r'Pending Beatmaps \([0-9]+\)')
+    GRAVEYARDED_ = re.compile(r'Graveyarded Beatmaps \([0-9]+\)')
+    PP_RANK_ = re.compile(r'\"pp_rank\":\"([0-9]+)\"')
+    CREATOR_ = re.compile(r'\"creator\":\"([^\"]+)\"')
+    INVALID_CHARACTERS_ = re.compile(r'[\\/\?:\*<>|"]')
 
-FIRST_PAGE = 1
-LAST_PAGE = 1
+    FIRST_PAGE = 1
+    LAST_PAGE = 1
 
-MIN_FAVOURITED = 5
-MIN_DIFFICULTY = 4.0
-MIN_RANKED = 1
-MIN_NON_RANKED = 5
-MIN_PP_RANK = 10000
+    MIN_FAVOURITED = 5
+    MIN_DIFFICULTY = 4.0
+    MIN_RANKED = 1
+    MIN_NON_RANKED = 5
+    MIN_PP_RANK = 10000
 
-DOWNLOAD_FOLDER = r'C:\Users\user\Desktop\beatmaps'
-ERROR_LOG = open('error_log.txt', 'w')
+    DOWNLOAD_FOLDER = r'C:\Users\user\Desktop\beatmaps'
+    ERROR_LOG = open('error_log.txt', 'w')
 
 
 class Beatmap:
@@ -73,8 +78,8 @@ def main():
             except Exception as err:
                 error_msg('Main: Error while downloading maps.', err)
                 return
-        except RequestException as err:
-            error_msg('Main: Could not scrape data properly.', err)
+        except Exception as err:
+            error_msg('Main: Unkown error while scraping data.', err)
             return
         finally:
             try:
@@ -83,13 +88,98 @@ def main():
                 error_msg('Main: Could not log data.', err)
     except RequestException as err:
         error_msg('Main: Could not open session.', err)
-        return
     finally:
         try:
             session.close()
         except RequestException as err:
             error_msg('Main: Could not close session.', err)
-            return
+
+
+def login(session):
+    session.post('https://osu.ppy.sh/forum/ucp.php?mode=login', data=LOGIN_DATA)
+
+
+
+def scrape_data(session, beatmaps):
+    scrape_beatmaps_id(session, beatmaps)
+    scrape_beatmaps_source(session, beatmaps)
+    scrape_beatmaps_json(session, beatmaps)
+    scrape_beatmaps_creator(session, beatmaps)
+    scrape_beatmaps_profile(session, beatmaps)
+    scrape_beatmaps_user_page(session, beatmaps)
+
+
+def scrape_beatmaps_id(session, beatmaps):
+    for page in range(FIRST_PAGE, LAST_PAGE + 1):
+        try:
+            response = session.get('https://osu.ppy.sh/p/beatmaplist?l=1&r=4&q=&g=0&la=0&s=4&o=1&m=-1&page='
+                                   + str(page))
+            try:
+                result = BEATMAP_ID_.findall(response.content)
+                for beatmap_id in result:
+                    beatmaps.append(Beatmap(beatmap_id))
+            except Exception as err:
+                error_msg('scrape_beatmaps_id: Error finding ids on page.', err)
+        except RequestException as err:
+            error_msg('scrape_beatmaps_id: Error getting page ' + str(page) + '.', err)
+
+
+
+def scrape_beatmaps_source(session, beatmaps):
+    for beatmap in beatmaps:
+        try:
+            response = session.get('https://osu.ppy.sh/s/'
+                                   + beatmap.id_)
+            beatmap.source = response.content
+        except RequestException as err:
+            error_msg('scrape_beatmaps_source: Error getting source page on beatmap '
+                      + str(beatmap.id_) + '.', err)
+
+
+# TODO
+# ADD METHOD TO SCRAPE KUDOSU FOR USER PAGE
+# NEED TO FAKE JAVASCRIPT
+def scrape_beatmaps_user_page(session, beatmaps):
+    pass
+
+
+def scrape_beatmaps_creator(session, beatmaps):
+    for beatmap in beatmaps:
+        creator = CREATOR_.search(beatmap.json).group(1)
+        response = session.get('https://osu.ppy.sh/api/get_user?k=c5878839513d6eb99dbf09f8244653332b93eb3c&u='
+                               + creator)
+        beatmap.creator = response.content
+
+
+def scrape_beatmaps_profile(session, beatmaps):
+    for beatmap in beatmaps:
+        try:
+            user_id = USER_ID_.search(beatmap.creator).group(1)
+            try:
+                response = session.get('https://osu.ppy.sh/pages/include/profile-beatmaps.php?u='
+                                       + user_id + '&m=0')
+                beatmap.profile = response.content
+            except RequestException as err:
+                error_msg('scrape_beatmaps_profile: Could not get profile page of creator '
+                          + str(user_id) + '.', err)
+        except Exception as err:
+            error_msg('scrape_beatmaps_profile: Error finding user_id of creator of beatmap '
+                      + beatmap.id_ + '.', err)
+
+
+
+
+
+
+def scrape_beatmaps_json(session, beatmaps):
+    for beatmap in beatmaps:
+        try:
+            response = session.get('https://osu.ppy.sh/api/get_beatmaps?k=c5878839513d6eb99dbf09f8244653332b93eb3c&s='
+                                   + beatmap.id_)
+            beatmap.json = response.content
+        except RequestException as err:
+            error_msg('scrape_beatmaps_json: Error getting json on beatmap '
+                      + str(beatmap.id_) + '.', err)
 
 
 def download_good_maps(session, beatmaps):
@@ -158,12 +248,20 @@ def ok_creator(beatmap):
 
 
 def ok_pp_rank(beatmap):
-    pp_rank = PP_RANK_.search(beatmap.creator).group(1)
-    return int(pp_rank) < MIN_PP_RANK
+    try:
+        pp_rank = PP_RANK_.search(beatmap.creator).group(1)
+        return int(pp_rank) <= MIN_PP_RANK
+    except Exception as err:
+        error_msg('ok_pp_rank: Could not find pp_rank regular expression in creator.', err)
+        return False
 
 
 def ok_maps(beatmap):
-    return ok_ranked(beatmap) or ok_non_ranked(beatmap)
+    try:
+        return ok_ranked(beatmap) or ok_non_ranked(beatmap)
+    except Exception as err:
+        error_msg('ok_maps: ok_ranked or ok_non_ranked returned unknown error.', err)
+        return False
 
 # TODO
 # FIRST FINISH SCRAPE_BEATMAPS_USER_PAGE
@@ -193,106 +291,92 @@ def ok_non_ranked(beatmap):
     return int(pending) + int(graveyarded) >= MIN_NON_RANKED
 
 
+def open_log_files():
+    try:
+        json_file = open('json.txt', 'w')
+    except Exception as err:
+        error_msg('open_log_files: Could not open json_file.', err)
+        json_file = None
+    try:
+        source_file = open('source.txt', 'w')
+    except Exception as err:
+        error_msg('open_log_files: Could not open source_file.', err)
+        source_file = None
+    try:
+        creator_file = open('creator.txt', 'w')
+    except Exception as err:
+        error_msg('open_log_files: Could not open creator_file.', err)
+        creator_file = None
+    try:
+        profile_file = open('profile.txt', 'w')
+    except Exception as err:
+        error_msg('open_log_files: Could not open profile_file.', err)
+        profile_file = None
+    try:
+        user_page_file = open('user_page.txt', 'w')
+    except Exception as err:
+        error_msg('open_log_files: Could not open user_page file.', err)
+        user_page_file = None
+
+    return (json_file, source_file, creator_file, profile_file, user_page_file)
+
+
+def close_log_files(json_file, source_file, creator_file, profile_file, user_page_file):
+    try:
+        json_file.close()
+    except Exception as err:
+        error_msg('close_log_files: Could not close json_file.', err)
+    try:
+        source_file.close()
+    except Exception as err:
+        error_msg('close_log_files: Could not close source_file.', err)
+    try:
+        creator_file.close()
+    except Exception as err:
+        error_msg('close_log_files: Could not close creator_file.', err)
+    try:
+        profile_file.close()
+    except Exception as err:
+        error_msg('close_log_files: Could not close profile_file.', err)
+    try:
+        user_page_file.close()
+    except Exception as err:
+        error_msg('close_log_files: Could not close user_page_file.', err)
+
+
 def log_data(beatmaps):
-    json_file = open('json.txt', 'w')
-    source_file = open('source.txt', 'w')
-    creator_file = open('creator.txt', 'w')
-    profile_file = open('profile.txt', 'w')
-    user_page_file = open('user_page.txt', 'w')
+    json_file, source_file, creator_file, profile_file, user_page_file = open_log_files()
+
     for beatmap in beatmaps:
-        json_file.write(beatmap.json + '\n')
-        source_file.write(beatmap.source + '\n')
-        creator_file.write(beatmap.creator + '\n')
-        profile_file.write(beatmap.profile + '\n')
-        user_page_file.write(beatmap.user_page + '\n')
-
-    json_file.close()
-    source_file.close()
-    creator_file.close()
-    profile_file.close()
-    user_page_file.close()
-
-
-def scrape_data(session, beatmaps):
-    scrape_beatmaps_id(session, beatmaps)
-    scrape_beatmaps_source(session, beatmaps)
-    scrape_beatmaps_json(session, beatmaps)
-    scrape_beatmaps_creator(session, beatmaps)
-    scrape_beatmaps_profile(session, beatmaps)
-    scrape_beatmaps_user_page(session, beatmaps)
-
-
-# TODO
-# ADD METHOD TO SCRAPE KUDOSU FOR USER PAGE
-# NEED TO FAKE JAVASCRIPT
-def scrape_beatmaps_user_page(session, beatmaps):
-    for beatmap in beatmaps:
-        beatmap.user_page = '\n'
-
-
-def scrape_beatmaps_creator(session, beatmaps):
-    for beatmap in beatmaps:
-        creator = CREATOR_.search(beatmap.json).group(1)
-        response = session.get('https://osu.ppy.sh/api/get_user?k=c5878839513d6eb99dbf09f8244653332b93eb3c&u='
-                               + creator)
-        beatmap.creator = response.content
-
-
-def scrape_beatmaps_profile(session, beatmaps):
-    for beatmap in beatmaps:
-        try:
-            user_id = USER_ID_.search(beatmap.creator).group(1)
+        if json_file is not None:
             try:
-                response = session.get('https://osu.ppy.sh/pages/include/profile-beatmaps.php?u='
-                                       + user_id + '&m=0')
-                beatmap.profile = response.content
-            except RequestException as err:
-                error_msg('scrape_beatmaps_profile: Could not get profile page of creator '
-                          + str(user_id) + '.', err)
-        except Exception as err:
-            error_msg('scrape_beatmaps_profile: Error finding user_id of creator of beatmap '
-                      + beatmap.id_ + '.', err)
-
-
-def scrape_beatmaps_id(session, beatmaps):
-    for page in range(FIRST_PAGE, LAST_PAGE + 1):
-        try:
-            response = session.get('https://osu.ppy.sh/p/beatmaplist?l=1&r=4&q=&g=0&la=0&s=4&o=1&m=-1&page='
-                                   + str(page))
-            try:
-                result = BEATMAP_ID_.findall(response.content)
-                for beatmap_id in result:
-                    beatmaps.append(Beatmap(beatmap_id))
+                json_file.write(beatmap.json + '\n')
             except Exception as err:
-                error_msg('scrape_beatmaps_id: Error finding ids on page.', err)
-        except RequestException as err:
-            error_msg('scrape_beatmaps_id: Error getting page ' + str(page) + '.', err)
+                error_msg('log_data: Could not write to json_file.', err)
+        if source_file is not None:
+            try:
+                source_file.write(beatmap.source + '\n')
+            except Exception as err:
+                error_msg('log_data: Could not write to source_file.', err)
+        if creator_file is not None:
+            try:
+                creator_file.write(beatmap.creator + '\n')
+            except Exception as err:
+                error_msg('log_data: Could not write to creator_file.', err)
+        if profile_file is not None:
+            try:
+                profile_file.write(beatmap.profile + '\n')
+            except Exception as err:
+                error_msg('log_data: Could not write to profile_file.', err)
+        if user_page_file is not None:
+            try:
+                user_page_file.write(beatmap.user_page + '\n')
+            except Exception as err:
+                error_msg('log_data: Could not write to user_page_file.', err)
+
+    close_log_files(json_file, source_file, creator_file, profile_file, user_page_file)
 
 
-def scrape_beatmaps_source(session, beatmaps):
-    for beatmap in beatmaps:
-        try:
-            response = session.get('https://osu.ppy.sh/s/'
-                                   + beatmap.id_)
-            beatmap.source = response.content
-        except RequestException as err:
-            error_msg('scrape_beatmaps_source: Error getting source page on beatmap '
-                      + str(beatmap.id_) + '.', err)
-
-
-def scrape_beatmaps_json(session, beatmaps):
-    for beatmap in beatmaps:
-        try:
-            response = session.get('https://osu.ppy.sh/api/get_beatmaps?k=c5878839513d6eb99dbf09f8244653332b93eb3c&s='
-                                   + beatmap.id_)
-            beatmap.json = response.content
-        except RequestException as err:
-            error_msg('scrape_beatmaps_json: Error getting json on beatmap '
-                      + str(beatmap.id_) + '.', err)
-
-
-def login(session):
-    session.post('https://osu.ppy.sh/forum/ucp.php?mode=login', data=LOGIN_DATA)
 
 
 def download_beatmap(session, beatmap):
@@ -356,6 +440,12 @@ def error_msg(msg, err):
 
 if __name__ == "__main__":
     try:
-        main()
-    finally:
-        ERROR_LOG.close()
+        init()
+    except Exception as error:
+        print('Could not initialize variables.')
+        print(str(error))
+    else:
+        try:
+            main()
+        finally:
+            ERROR_LOG.close()
